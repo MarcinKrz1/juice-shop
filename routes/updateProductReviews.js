a48f437b35feb69b02d3c1a25fe306d67f0861b6
MarcinKrz <marokrzemo245@gmail.com>
2020-11-12 00:32:50 +0100

    repaired updateProductReviews

--- routes/updateProductReviews.js
+++ routes/updateProductReviews.js
@@ -7,10 +7,14 @@ const utils = require('../lib/utils')
 const challenges = require('../data/datacache').challenges
 const db = require('../data/mongodb')
 const insecurity = require('../lib/insecurity')
+const mongoSanitize = require('express-mongo-sanitize')
 
 module.exports = function productReviews () {
   return (req, res, next) => {
     const user = insecurity.authenticatedUsers.from(req)
+    mongoSanitize.sanitize(req.body.id, {
+      replaceWith: '_'
+    });
     db.reviews.update(
       { _id: req.body.id },
       { $set: { message: req.body.message } },
@@ -18,7 +22,9 @@ module.exports = function productReviews () {
     ).then(
       result => {
         utils.solveIf(challenges.noSqlReviewsChallenge, () => { return result.modified > 1 })
+        if(result.original.length>0){
         utils.solveIf(challenges.forgedReviewChallenge, () => { return user && user.data && result.original[0].author !== user.data.email && result.modified === 1 })
+        }
         res.json(result)
       }, err => {
         res.status(500).json(err)
